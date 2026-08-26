b098d6309cab573c46a774a9ce3771e767789c1c
Jagadeesh <jaggu_1980@yahoo.co.in>
2026-08-26 17:29:50 +0000

    fix: resolve terraform IAM service account reference and bump GKE RAM limits

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,5 +1,6 @@
+# terraform/iam.tf
 resource "google_project_iam_member" "gke_cloud_trace_user" {
   project = var.gcp_project_id
   role    = "roles/cloudtrace.agent"
-  member  = "serviceAccount:${google_service_account.oneshield_sa.email}"
+  member  = "serviceAccount:oneshield-terraform-sa@${var.gcp_project_id}.iam.gserviceaccount.com"
 }
